9e622fd4840af49a839fe6a77fca5aba60a339a7
KhorunzhiiVV <vladimir.Khorunzhii@gmail.com>
2023-03-20 21:33:04 +0300

    init commit

---
+++ src/test/java/ru/khorunzhii/pastecut/PastecutApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.khorunzhii.pastecut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastecutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
